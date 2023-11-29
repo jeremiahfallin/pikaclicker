dde508be9e2399d611e224753556650c1db24a22
Jeremiah Fallin <jeremiah.fallin@gmail.com>
2023-11-28 18:14:51 -0800

    add gyms

---
+++ src/constants/elite4.js
@@ -0,0 +1,60 @@
+import { createPokemon } from "@/utils";
+
+const elite4 = [
+  {
+    name: "",
+    type: "water",
+    leader: "",
+    town: "",
+    pokemon: [
+      createPokemon(186, 100), // Politoed
+      createPokemon(199, 100), // Slowking
+      createPokemon(134, 100), // Vaporeon
+      createPokemon(230, 100), // Kingdra
+      createPokemon(272, 100), // Ludicolo
+      createPokemon(350, 100), // Milotic
+    ],
+  },
+  {
+    name: "",
+    type: "fairy",
+    leader: "",
+    town: "",
+    pokemon: [
+      createPokemon(184, 100), // Azumarill
+      createPokemon(282, 100), // Gardevoir
+      createPokemon(700, 100), // Sylveon
+      createPokemon(959, 100), // Tinkaton
+      createPokemon(861, 100), // Grimmsnarl
+      createPokemon(730, 100), // Primarina
+    ],
+  },
+  {
+    name: "",
+    type: "psychic",
+    leader: "",
+    town: "",
+    pokemon: [
+      createPokemon(196, 100), // Espeon
+      createPokemon(475, 100), // Gallade
+      createPokemon(10172, 100), // Slowking-Galar
+      createPokemon(956, 100), // Espathra
+      createPokemon(65, 100), // Alakazam
+      //   createPokemon(), // Togekiss
+    ],
+  },
+  {
+    name: "",
+    type: "steel",
+    leader: "",
+    town: "",
+    pokemon: [
+      createPokemon(983, 100), // Kingambit
+      createPokemon(1000, 100), // Gholdengo
+      createPokemon(376, 100), // Metagross
+      createPokemon(823, 100), // Corviknight
+      createPokemon(303, 100), // Mawile
+      createPokemon(706, 100), // Goodra
+    ],
+  },
+];
--- src/constants/gyms.js
+++ src/constants/gyms.js
@@ -8,12 +8,12 @@ const gyms = [
     leader: "Yara",
     town: "Home",
     pokemon: [
-      createPokemon(1, 12),
-      createPokemon(15, 12),
-      createPokemon(285, 16),
-      createPokemon(44, 18),
-      createPokemon(470, 18),
-      createPokemon(10237, 18),
+      createPokemon(1, 12), // Bulbasaur
+      createPokemon(15, 12), // Beedrill
+      createPokemon(285, 16), // Shroomish
+      createPokemon(44, 18), // Gloom
+      createPokemon(470, 18), // Leafeon
+      createPokemon(10237, 18), // Liligant-Hisui
     ],
   },
   {
@@ -23,12 +23,12 @@ const gyms = [
     leader: "Richter",
     town: "Desert Town",
     pokemon: [
-      createPokemon(935, 20),
-      createPokemon(78, 20),
-      createPokemon(59, 24),
-      createPokemon(136, 24),
-      createPokemon(851, 28),
-      createPokemon(910, 28),
+      createPokemon(935, 20), // Charcadet
+      createPokemon(78, 20), // Rapidash
+      createPokemon(59, 24), // Arcanine
+      createPokemon(136, 24), // Flareon
+      createPokemon(851, 28), // Centiskorch
+      createPokemon(910, 28), // Crocalor
     ],
   },
   {
@@ -38,12 +38,12 @@ const gyms = [
     leader: "",
     town: "Swamp Town",
     pokemon: [
-      createPokemon(93, 32),
-      createPokemon(980, 32),
-      createPokemon(609, 32),
-      createPokemon(24, 34),
-      createPokemon(302, 36),
-      createPokemon(10113, 36),
+      createPokemon(93, 32), // Haunter
+      createPokemon(980, 32), // Clodsire
+      createPokemon(609, 32), // Chandelure
+      createPokemon(24, 34), // Arbok
+      createPokemon(302, 36), // Sableye
+      createPokemon(10113, 36), // Muk-Alola
     ],
   },
   {
@@ -53,13 +53,86 @@ const gyms = [
     leader: "",
     town: "Rock Town",
     pokemon: [
-      createPokemon(10230, 40),
-      createPokemon(213, 42),
-      createPokemon(248, 44),
-      createPokemon(558, 44),
-      createPokemon(306, 46),
-      createPokemon(464, 48),
+      createPokemon(10230, 40), // Arcanine-Hisui
+      createPokemon(213, 42), // Shuckle
+      createPokemon(248, 44), // Tyranitar
+      createPokemon(558, 44), // Crustle
+      createPokemon(306, 46), // Aggron
+      createPokemon(464, 50), // Rhyperior
+    ],
+  },
+  {
+    name: "Electric",
+    type: "electric",
+    badge: "Electric",
+    leader: "",
+    town: "Electric Town",
+    pokemon: [
+      createPokemon(26, 54), // Raichu-Alola
+      createPokemon(404, 54), // Luxray
+      createPokemon(135, 56), // Jolteon
+      createPokemon(778, 58), // Mimikyu
+      createPokemon(596, 58), // Galvantula
+      createPokemon(181, 60), // Ampharos
+    ],
+  },
+  {
+    name: "Desert",
+    type: "ground",
+    badge: "Desert",
+    leader: "",
+    town: "Desert Town",
+    pokemon: [
+      createPokemon(34, 62), // Nidoking
+      createPokemon(31, 64), // Nidoqueen
+      createPokemon(248, 66), // Tyranitar
+      createPokemon(445, 68), // Garchomp
+      createPokemon(553, 68), // Krookodile
+      createPokemon(330, 70), // Flygon
+    ],
+  },
+  {
+    name: "Flying",
+    type: "flying",
+    badge: "Flying",
+    leader: "",
+    pokemon: [
+      createPokemon(130, 72), // Gyarados
+      createPokemon(142, 72), // Aerodactyl
+      createPokemon(149, 73), // Dragonite
+      createPokemon(169, 74), // Crobat
+      createPokemon(962, 75), // Bombirdier
+      createPokemon(334, 76), // Altaria
+    ],
+  },
+  {
+    name: "Ice",
+    type: "ice",
+    badge: "Ice",
+    leader: "",
+    pokemon: [
+      // createPokemon(), // Sandslash-alola
+      createPokemon(10104, 80), // Ninetales-alola
+      createPokemon(873, 82), //Frosmoth
+      createPokemon(471, 86), //Glaceon
+      createPokemon(131, 86), //lapras
+      createPokemon(478, 88), //Froslass
+    ],
+  },
+  {
+    name: "Fossil",
+    type: "rock",
+    badge: "Fossil",
+    leader: "",
+    pokemon: [
+      createPokemon(139, 90), // Omastar
+      createPokemon(141, 92), // Kabutops
+      createPokemon(348, 94), // Armaldo
+      // createPokemon(565, 96), // Archeops
+      // createPokemon(10105, 98), // Aurorus
+      // createPokemon(10106, 100), // Cranidos
     ],
   },
 ];
+
 export default gyms;
