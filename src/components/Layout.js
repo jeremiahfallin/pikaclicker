66a95e304d6d25c58606d1edee7ca22fc9b3fc50
Jeremiah Fallin <jeremiah.fallin@gmail.com>
2023-12-11 17:59:44 -0800

    elite4

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,14 @@
 import { signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth";
-import { Box, Center, Link } from "@chakra-ui/react";
+import { Box, Button, Center, Link, useClipboard } from "@chakra-ui/react";
 import { auth } from "@/config";
 import useAuth from "@/hooks/useAuth";
+import useGameStore from "@/hooks/useGameStore";
+import SuperJSON from "superjson";
 
 export default function Layout({ children }) {
   const { user } = useAuth();
+  const data = useGameStore();
+  const clipboard = useClipboard(SuperJSON.stringify(data));
   const onClick = () => {
     if (user) {
       signOut(auth);
@@ -13,9 +17,16 @@ export default function Layout({ children }) {
     }
   };
 
+  const copy = () => {
+    clipboard.onCopy();
+  };
+
   return (
     <Box>
       <Center bg="blue.200" justifyContent={"end"} p={1}>
+        <Button size="xs" onClick={copy}>
+          Copy
+        </Button>
         <Link as="button" type="button" onClick={onClick} fontSize="xs">
           {user ? "Sign Out" : "Sign In"}
         </Link>
